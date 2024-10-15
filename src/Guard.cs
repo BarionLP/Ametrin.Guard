0536494b797aa830c3159a09798a28010d8ff350
Barion <ametrin.studios@gmail.com>
2024-10-15 16:43:33 +0200

    ThrowIfNullOrEmpty for collections

--- src/Guard.cs
+++ src/Guard.cs
@@ -9,6 +9,10 @@ public static class Guard
     public static T ThrowIfNull<T>(T? value, [CallerArgumentExpression(nameof(value))] string valueExpression = "") where T : class
         => value is null ? throw new ArgumentNullException(valueExpression) : value;
 
+    public static T ThrowIfNullOrEmpty<T>(T? collection, [CallerArgumentExpression(nameof(collection))] string valueExpression = "") where T : System.Collections.IEnumerable 
+        => collection is not null && collection.GetEnumerator().MoveNext() ? collection : throw new ArgumentNullException(valueExpression);
+
+
     [StackTraceHidden]
     public static string ThrowIfNullOrEmpty(string? value, [CallerArgumentExpression(nameof(value))] string valueExpression = "")
         => string.IsNullOrEmpty(value) ? throw new ArgumentNullException(valueExpression) : value;
